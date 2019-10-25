fce0ae2d38db6053088a36d44177f0399bdab996
Student <example@example.com>
2019-10-25 09:45:03 -0700

    began work on answer-selection logic, using point based system

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var languages = ["Ruby","C#","Javascript","Go, like the game not the language","Python"];
   $("#calc").submit(function(event) {
     var input = $("input#string").val();
     console.log(input);
@@ -7,12 +8,23 @@ $(document).ready(function() {
   });
   $("form#codeform").submit(function(event) {
     var id = $("input#id").val();
+    var dob = $("input#dob").val();
+    var num0 = $("input#num0").val();
+    var results = logic(id,dob,num0);
 
     $(".id").text(id);
 
     event.preventDefault();
   });
 });
+function logic(id, dob, num0, num1, num2) {
+  var points = [0,0,0,0,0,0]; // point value toward each language
+  var returns[];
+  if (Math.cos(num0) > Math.cos(num1)) {
+    points[4] += 3;
+    points[6] -= 4;
+  }
+}
 function resolve(num) {
   var returnMe = eval(num);
   return returnMe;

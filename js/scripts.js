6042f8cd37f27b47ce005c5db65f8feb426559b7
Student <example@example.com>
2019-10-25 08:59:32 -0700

    working on project logic behind revealing new questions when data is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+  $("input.form-control").oninput= function() {
+    console.log("Success");
+  }
   $("form#codeform").submit(function(event) {
     var id = $("input#id").val();
 
     $(".id").text(id);
 
     event.preventDefault();
-  })
-})
+  });
+
+  function reset(){
+    // Resets the response, lets you answer again
+  }
+});
